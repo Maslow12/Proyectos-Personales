8583e7585e945013071680cf0eb1c1a4681cd218
Sebastian <40075601+Maslow12@users.noreply.github.com>
2022-05-04 11:46:03 -0400

    Update import_file.py

--- import_file_web/import_file.py
+++ import_file_web/import_file.py
@@ -1,13 +1,16 @@
 import requests
 import os
-def import_file(url,extension,filename='file',directory='')->str():
-    r = requests.get(url, allow_redirects=True)
-    f = f'{filename}.{extension}'
-    if directory == '':
-        save_path = os.getcwd()
-    else:
-        save_path = directory
-    open(f, 'wb').write(r.content)
-    folder = os.path.join(save_path, f)
-    return folder
-print(import_file('https://www.cbssports.com/nba/standings/', 'csv'))
+def import_file(url,extension,filename='file',directory=''):
+    try:
+        r = requests.get(url, allow_redirects=True)
+        f = f'{filename}.{extension}'
+        if directory == '':
+            save_path = os.getcwd()
+        else:
+            save_path = directory
+        open(f, 'wb').write(r.content)
+        folder = os.path.join(save_path, f)
+        return folder
+    except requests.ConnectionError:
+        raise Exception('No hay conexion...')
+print(import_file('https://www.cbssports.com/nba/standings/', 'html'))
