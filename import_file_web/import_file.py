daaa5172123936dc87995fde3ce4050799b72fd6
Sebastian <40075601+Maslow12@users.noreply.github.com>
2022-05-06 22:51:29 -0400

    Update import_file.py

--- import_file_web/import_file.py
+++ import_file_web/import_file.py
@@ -1,16 +1,18 @@
 import requests
 import os
-def import_file(url,extension,filename='file',directory=''):
+def import_file(url,extension,filename='file',directory='', n=1):
     try:
-        r = requests.get(url, allow_redirects=True)
-        f = f'{filename}.{extension}'
-        if directory == '':
-            save_path = os.getcwd()
-        else:
-            save_path = directory
-        open(f, 'wb').write(r.content)
-        folder = os.path.join(save_path, f)
+        for url in url:
+            r = requests.get(url, allow_redirects=True)
+            f = f'{filename}_{n}.{extension}'
+            if directory == '':
+                save_path = os.getcwd()
+            else:
+                save_path = directory
+            open(f, 'wb').write(r.content)
+            folder = os.path.join(save_path, f)
+            print(f'Listo archivo {n}...')
+            n+=1
         return folder
     except requests.ConnectionError:
         raise Exception('No hay conexion...')
-print(import_file('https://www.cbssports.com/nba/standings/', 'html'))
